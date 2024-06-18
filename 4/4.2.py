e2687aa9d45cfd75c3f3c8725294c65f4deb5446
bel1ves <148337478+bel1ves@users.noreply.github.com>
2024-06-18 18:11:44 +0300

    Add

---
+++ 4/4.2.py
@@ -0,0 +1,12 @@
+class Employee:
+    company = "Industries Co."
+    status = None
+    task = None
+
+arthur = Employee
+arthur.salary = 3600
+arthur.name = "Arthur"
+
+edwin = Employee
+edwin.salary = 3600
+edwin.name = "Edwin"
